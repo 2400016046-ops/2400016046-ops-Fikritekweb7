ae09b600eabffbc4a86bceb8d62ad3f386007107
FIKRI ILHAM <2400016046@webmail.uad.ac.id>
2025-12-12 22:22:48 +0700

    update

---
+++ web7/src/pages/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="container mx-auto px-10 py-20 text-center">
+      Ruang ekspresi adalah media untuk mengekspresikan diri secara bebas, kreatif, dan bertanggung jawab.
+    </div>
+  );
+}
---
+++ web7/src/pages/Testimony.jsx
@@ -0,0 +1,7 @@
+ export default function Testimony() {
+  return (
+    <div className="container mx-auto px-6 py-20 text-center">
+      Halaman Testimony
+    </div>
+  );
+}
